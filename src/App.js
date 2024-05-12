1845de39bfa084f222c1a0567e00547a5d48b079
mmazzo <mjm2z@outlook.com>
2024-05-12 12:56:23 -0400

    adding components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { styled } from '@stitches/react';
+import { Market } from './components/market';
+
+const AppContainer = styled('div', {
+	display: 'flex',
+	justifyContent: 'center',
+	alignItems: 'center',
+	height: '100vh',
+
+	// alignItems: 'center',
+	// alignContent: 'center',
+});
+
 function App() {
-	return <div>init</div>;
+	return (
+		<AppContainer>
+			{/* <Todos /> */}
+			<Market />
+		</AppContainer>
+	);
 }
 
 export default App;
